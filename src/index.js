62a56a1cafa57341cef5735c6356af904148bace
Dario Cruz <rio2012@gmail.com>
2023-04-16 14:55:54 -0400

    Change single button to form for location input, add attributeHelper npm package.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import {attributeHelper} from '@dariocru/attribute-helper'
 import { getWeatherData } from './helpers/getWeatherData'
+import attributeHelper from '@dariocru/attribute-helper'
 
 // Define elements needed for site. 
 const getWeatherButton = document.createElement('button')
 getWeatherButton.innerText = 'Log the weather!'
-
+attributeHelper(getWeatherButton, {
+    'type':'submit',
+})
 
 // Define an input element so that the user can input there location and get the weather.
 const locationInput = document.createElement('input')
+attributeHelper(locationInput, {
+    'id':'location-input',
+    'placeholder':'Enter a location.',
+})
 
+// Form element for inputs and button
+const weatherForm = document.createElement('form')
+attributeHelper(weatherForm, {
+    'action':'',
+    'id':'weather-form',
+})
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let locationInfo = locationInput.value
+    getWeatherData(locationInfo)
+})
 
-// On click run the get weather function.
-getWeatherButton.addEventListener('click', getWeatherData)
 
 // Append the button to the dom for testing.
-document.body.appendChild(getWeatherButton)
+weatherForm.append(locationInput, getWeatherButton)
+document.body.appendChild(weatherForm)
 
