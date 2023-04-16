d8e97ba75b9f9e2ed5e8aaa681a852d8e97e18f2
Dario Cruz <rio2012@gmail.com>
2023-04-16 10:02:26 -0400

    Add defer to scriptl tag in index.html, Add innerText to button element.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Define elements needed for site. 
 const getWeatherButton = document.createElement('button')
 
+getWeatherButton.innerText = 'Log the weather!!'
 
 async function getWeather() {
     const weatherData = await fetch('https://api.weatherapi.com/v1/current.json?key=46584a9caf1f4b8887b153831231204&q=london', {mode : 'cors'})
@@ -11,5 +12,5 @@ async function getWeather() {
 getWeatherButton.addEventListener('click', getWeather)
 
 // Append the button to the dom for testing.
-document.body.append(getWeatherButton)
+document.body.appendChild(getWeatherButton)
 
