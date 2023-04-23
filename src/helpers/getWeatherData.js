e7dcc51efb9dc60834255ba7cfbc69707e61c060
Dario Cruz <rio2012@gmail.com>
2023-04-23 11:03:16 -0400

    Add folders for content, add comments to getweatherData.js

--- src/helpers/getWeatherData.js
+++ src/helpers/getWeatherData.js
@@ -1,7 +1,12 @@
+// Get weather data with passed string.
 async function getWeatherData(locationString) {
+    // Call api and get promise.
     const apiResponse = await fetch(`https://api.weatherapi.com/v1/current.json?key=46584a9caf1f4b8887b153831231204&q=${locationString}`, {mode : 'cors'})
+    // On promise retrieval, process to object.
     const weatherData = await apiResponse.json()
     console.log(weatherData)
+    // Return object data. 
+    return weatherData
 }
 
 
