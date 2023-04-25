40d23476b350f681856dc294b359e2cc05da916b
Dario Cruz <rio2012@gmail.com>
2023-04-25 12:55:13 -0400

    Remove testpage.js no longer needed.

--- src/index.js
+++ src/index.js
@@ -3,36 +3,6 @@ import { getWeatherData } from './helpers/getWeatherData'
 import { getUserLocation } from './helpers/getLocation'
 import { weatherFormEvent } from './pageComponents/events'
 
-// Define elements needed for site. 
-const getWeatherButton = document.createElement('button')
-getWeatherButton.innerText = 'Log the weather!'
-attributeHelper(getWeatherButton, {
-    'type':'submit',
-})
 
-// Define an input element so that the user can input there location and get the weather.
-const locationInput = document.createElement('input')
-attributeHelper(locationInput, {
-    'id':'location-input',
-    'placeholder':'Enter a location.',
-})
-
-// Form element for inputs and button
-const weatherForm = document.createElement('form')
-attributeHelper(weatherForm, {
-    'action':'',
-    'id':'weather-form',
-})
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let locationInfo = locationInput.value
-    locationInput.value = ''
-    getWeatherData(locationInfo)
-})
-
-
-// Append the button to the dom for testing.
-weatherForm.append(locationInput, getWeatherButton)
-document.body.appendChild(weatherForm)
 
 getUserLocation()
